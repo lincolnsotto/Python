7506d297f9f68c2a8863e23f4d9ab871102b66f3
lincolnsotto <lincoln.sotto@hotmail.com>
2019-08-29 20:54:05 -0300

    29/08/2019

--- 04_Livro_Automatizando_tarefas_macantes_com_python/cap_6_strings_password.py
+++ 04_Livro_Automatizando_tarefas_macantes_com_python/cap_6_strings_password.py
@@ -1,20 +1,27 @@
 #!/Users/macbookair/.conda/envs/Git/bin/python3
 
-# A program to save passwords [IT IS NOT SECURITY].
+# A program to save passwords IT IS NOT SECURITY.
 
 passwords = {"email": "testEMAILpassword",
              "blog": "testBLOGpassword",
              "luggage": "testLUGGAGEpassword"}
 
 import sys, pyperclip
+
+print("What's password do you look?")
+sys.argv = input()
+
+
 if len(sys.argv) < 2:
     print("Usage: python Password Program [account] - copy account password")
     sys.exit()
 
-account = sys.argv[1]  # the first argument of command line is the name of account
+account = sys.argv[:]  # the first argument of command line is the name of account
 
 if account in passwords:
     pyperclip.copy(passwords[account])
     print("Password for " + account + "copied to clipboard")
 else:
     print("There ins't account named " + account)
+
+
